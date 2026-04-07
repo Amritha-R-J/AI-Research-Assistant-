5454883d4c4f34b43b0914acedab99f443a3dc8e
Amritha-R-J <100410005+Amritha-R-J@users.noreply.github.com>
2026-04-08 02:26:23 +0530

    Update content_writer.py

--- agents/content_writer.py
+++ agents/content_writer.py
@@ -2,6 +2,12 @@ import os
 from crewai import Agent, LLM
 from crewai_tools import FileWriterTool
 
+import os
+import streamlit as st
+
+def get_secret(key):
+    return os.getenv(key) or st.secrets.get(key)
+
 
 # LLM configurations - Agent specific config
 model = os.getenv("WRITER_AGENT_LLM")
